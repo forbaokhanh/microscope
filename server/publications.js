4cf49d5281594e2b99b34fb3c5c0fa17d1484692
Bao Khanh Tran <tranthibaokhanh@gmail.com>
2015-03-22 15:00:33 -0700

    1

--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,12 @@
-Meteor.publish('posts', function() {
+Meteor.publish('posts', function(author) {
   return Posts.find();
+});
+
+Meteor.publish('comments', function(postId) {
+  check(postId, String);
+  return Comments.find({postId: postId});
+});
+
+Meteor.publish('notifications', function() {
+  return Notifications.find({userId: this.userId, read: false});
 });
